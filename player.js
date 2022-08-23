7d1d0332cf94afafccafc7b0cf79f83ab68dd990
abusayed1 <mdabusayed72933sayed@gmail.com>
2022-08-23 23:55:59 +0600

    first commit

---
+++ player.js
@@ -0,0 +1,59 @@
+// fucntion for getting vlaue 
+
+function getValue(feildID){
+    const getField = document.getElementById(feildID)
+    const getFieldString = getField.value
+    const getValue = parseInt(getFieldString)
+    getField.value = ''
+    return getValue
+}
+
+
+
+// function for getting inner text
+
+function getInnerText(feildID){
+    const getField = document.getElementById(feildID)
+    const getFieldString = getField.innerText
+    const getInnerText = parseInt(getFieldString)
+    return getInnerText
+}
+
+// fuction for setting a vlaue
+
+function setValue(value, id){
+    const getValue = document.getElementById(id)
+    getValue.innerText = value
+}
+
+
+
+// function for add event listner
+
+function addEventListener(fieldID, listPlayerName){
+    document.getElementById(fieldID).addEventListener('click', function(){
+
+        const button = document.getElementById(fieldID)
+        button.setAttribute('disabled', true) 
+    
+        const playerName = document.getElementById(listPlayerName)
+        const innerText = playerName.innerText
+
+        const playerList = document.getElementById('player-list')
+        const timeCount = document.getElementsByTagName('li').length
+
+        if(timeCount < 5){  
+            const creat = document.createElement('li')
+            creat.innerText = innerText
+            playerList.appendChild(creat)
+
+        }  else{
+            alert('Vacancy is full. You cant add more player in this card. ')
+
+        }
+        
+    
+    
+    })
+}
+
---
+++ player1.js
@@ -0,0 +1,37 @@
+document.getElementById('calculate-button').addEventListener('click', function(){
+    const playerPrice = getValue('player-amount')
+
+    if(isNaN(playerPrice)){
+        alert('Please give a number value to continue.')
+        return
+    }
+
+    const timeCount = document.getElementsByTagName('li').length
+    const allPlayerCost = playerPrice * timeCount
+    setValue(allPlayerCost, 'player-ex' )
+
+})
+
+
+
+document.getElementById('total-calculate').addEventListener('click', function(){
+    const totalPlayerCost = getInnerText('player-ex')
+    const managerCost = getValue('manager-cost')
+    const coachCost = getValue('coach-cost')
+
+    if(isNaN(managerCost + coachCost)){
+        alert('Please give a number value to continue.')
+        return
+    }
+
+    const finalTotal = totalPlayerCost + managerCost + coachCost
+    setValue(finalTotal, 'final-total' )
+
+})
+
+addEventListener('first-button', 'first-player')
+addEventListener('sec-button', 'sec-player')
+addEventListener('third-button', 'third-player')
+addEventListener('fourth-button', 'fourth-player')
+addEventListener('fifth-button', 'fifth-player')
+addEventListener('sixth-button', 'sixth-player')
